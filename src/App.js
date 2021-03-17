f5d3b1ed5fe2fbebfb073acd6c0a242ab9329579
nicolas <n.gaget69@gmail.com>
2021-03-17 21:50:30 +0100

    Refactorisation de l'application

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import "./App.css";
 import BeerDetails from "./features/beers/beer-details/BeerDetails";
 import BeerList from "./features/beers/beer-list/BeerList";
 import { Navbar } from "./components";
-function App() {
-  return (
-    <div className="App d-flex flex-column">
-      <Navbar />
-      <div className=" d-flex flex-row flex-fill pt-4 p-2">
-        <BeerList />
-        <BeerDetails />
+import { Component } from "react";
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      beers:[{
+        name: 'Leffe La Légère',
+        img:'https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzM1MDI5LTUyNzEzLmpwZyIsImVkaXRzIjp7IndlYnAiOnsicXVhbGl0eSI6ODB9LCJwbmciOnsicXVhbGl0eSI6ODB9LCJyZXNpemUiOnsid2lkdGgiOjc4NiwiaGVpZ2h0Ijo1NDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjAsImciOjAsImIiOjAsImFscGhhIjowfX19fQ==',
+        details: '5° alc | blonde',
+        description:"Robe jaune dorée coiffée d'une épaisse mousse blanche onctueuse."
+      }],
+      selectedBeer:0
+    }
+  }
+  render(){
+    return (
+      <div className="App d-flex flex-column">
+        <Navbar />
+        <div className=" d-flex flex-row flex-fill pt-4 p-2">
+          <BeerList />
+          <BeerDetails />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
--- src/features/beers/beer-list/BeerElement.js
+++ src/features/beers/beer-list/BeerElement.js
@@ -3,21 +3,44 @@ import React, { Component } from "react";
 export default class BeerElement extends Component {
   render() {
     return (
-      <div className="w-25 p-2">
-        <div className="card">
+      <div className="w-50 p-2">
+        <li className="card text-center carouse-item border-secondary">
+        <div
+          className="card-body p-2 d-flex flex-column"
+          
+        >
+          <h2 className="card-header mb-auto ">Iron Maiden Trooper - 33cl</h2>
           <img
+            className="img-fluid p-2 mt-auto"
             alt="bière"
             src="https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzc3MzYtNDI4ODguanBnIiwiZWRpdHMiOnsid2VicCI6eyJxdWFsaXR5Ijo4MH0sInBuZyI6eyJxdWFsaXR5Ijo4MH0sInJlc2l6ZSI6eyJ3aWR0aCI6Nzg2LCJoZWlnaHQiOjU0MCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MCwiZyI6MCwiYiI6MCwiYWxwaGEiOjB9fX19"
-            className="card-img-top"
+            
           />
-          <div className="card-body">
-            <h5 className="card-title">Iron Maiden Trooper - 33cl</h5>
-            <p className="card-text">
+        </div>
+        <div className="card-footer row row-cols-2 m-0">
+          <div className="col-md-6">
+            
+          </div>
+          <div className="col-md-6">Comment</div>
+        </div>
+      </li>
+        {/* <div className="border d-flex">
+          <img
+            
+            height="100"
+            alt="bière"
+            src="https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzc3MzYtNDI4ODguanBnIiwiZWRpdHMiOnsid2VicCI6eyJxdWFsaXR5Ijo4MH0sInBuZyI6eyJxdWFsaXR5Ijo4MH0sInJlc2l6ZSI6eyJ3aWR0aCI6Nzg2LCJoZWlnaHQiOjU0MCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MCwiZyI6MCwiYiI6MCwiYWxwaGEiOjB9fX19"
+            className="img"
+          />
+          <div className="flex-fill d-flex flex-column p-3">
+            <h5>Iron Maiden Trooper - 33cl</h5>
+            <hr className="w-100" />
+            <p>
               Robe blonde dorée plutôt classique surmontée toutefois d'une belle
               mousse blanche.
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
     );
   }
