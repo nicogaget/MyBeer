b0a2146e6429b606d658cc67db9eb9eb72f2989a
nicolas <n.gaget69@gmail.com>
2021-03-18 13:30:35 +0100

    Utilisation du rendu conditionnel et loader
    Utilisation de l'opérateur ternaire conditionnel
    Simulation du chargement

---
+++ src/data.js
@@ -0,0 +1,59 @@
+// eslint-disable-next-line 
+export default [
+  {
+    name: "Leffe La Légère",
+    img:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_StY6dI5YD9oMiE7M5Wu7jkcq2NuBUNaGTc6gsDB4s9edkQ-RBEP8nyg8bvM&usqp=CAc",
+    details: "5° alc | blonde",
+    description:
+      "Robe jaune dorée coiffée d'une épaisse mousse blanche onctueuse.",
+  },
+  {
+    name: "Leffe  Légère",
+    img:
+      "https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzM1MDI5LTUyNzEzLmpwZyIsImVkaXRzIjp7IndlYnAiOnsicXVhbGl0eSI6ODB9LCJwbmciOnsicXVhbGl0eSI6ODB9LCJyZXNpemUiOnsid2lkdGgiOjc4NiwiaGVpZ2h0Ijo1NDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjAsImciOjAsImIiOjAsImFscGhhIjowfX19fQ==",
+    details: "5° alc | blonde",
+    description:
+      "Robe jaune dorée coiffée d'une épaisse mousse blanche onctueuse.",
+  },
+  {
+    name: "Leffe La Légère",
+    img:
+      "https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzM1Nzk3LTU0NDk0LmpwZyIsImVkaXRzIjp7IndlYnAiOnsicXVhbGl0eSI6ODB9LCJwbmciOnsicXVhbGl0eSI6ODB9LCJyZXNpemUiOnsid2lkdGgiOjc4NiwiaGVpZ2h0Ijo1NDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjAsImciOjAsImIiOjAsImFscGhhIjowfX19fQ==",
+    details: "5° alc | blonde",
+    description:
+      "Robe jaune dorée coiffée d'une épaisse mousse blanche onctueuse.",
+  },
+  {
+    name: "Leffe La Légère",
+    img:
+      "https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzM1MDI5LTUyNzEzLmpwZyIsImVkaXRzIjp7IndlYnAiOnsicXVhbGl0eSI6ODB9LCJwbmciOnsicXVhbGl0eSI6ODB9LCJyZXNpemUiOnsid2lkdGgiOjc4NiwiaGVpZ2h0Ijo1NDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjAsImciOjAsImIiOjAsImFscGhhIjowfX19fQ==",
+    details: "5° alc | blonde",
+    description:
+      "Robe jaune dorée coiffée d'une épaisse mousse blanche onctueuse.",
+  },
+  {
+    name: "La Légère",
+    img:
+      "https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzM1MDI5LTUyNzEzLmpwZyIsImVkaXRzIjp7IndlYnAiOnsicXVhbGl0eSI6ODB9LCJwbmciOnsicXVhbGl0eSI6ODB9LCJyZXNpemUiOnsid2lkdGgiOjc4NiwiaGVpZ2h0Ijo1NDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjAsImciOjAsImIiOjAsImFscGhhIjowfX19fQ==",
+    details: "5° alc | brune",
+    description:
+      "Robe jaune dorée coiffée d'une épaisse mousse blanche onctueuse.",
+  },
+  {
+    name: "Leffe La Légère",
+    img:
+      "https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzgxODMtNTQ3MzIuanBnIiwiZWRpdHMiOnsid2VicCI6eyJxdWFsaXR5Ijo4MH0sInBuZyI6eyJxdWFsaXR5Ijo4MH0sInJlc2l6ZSI6eyJ3aWR0aCI6Nzg2LCJoZWlnaHQiOjU0MCwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MCwiZyI6MCwiYiI6MCwiYWxwaGEiOjB9fX19",
+    details: "5° alc | blonde",
+    description:
+      "Robe jaune dorée coiffée d'une épaisse mousse blanche onctueuse.",
+  },
+  {
+    name: "Leffe La Légère",
+    img:
+      "https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzM1MDI5LTUyNzEzLmpwZyIsImVkaXRzIjp7IndlYnAiOnsicXVhbGl0eSI6ODB9LCJwbmciOnsicXVhbGl0eSI6ODB9LCJyZXNpemUiOnsid2lkdGgiOjc4NiwiaGVpZ2h0Ijo1NDAsImZpdCI6ImNvbnRhaW4iLCJiYWNrZ3JvdW5kIjp7InIiOjAsImciOjAsImIiOjAsImFscGhhIjowfX19fQ==",
+    details: "5° alc | blonde",
+    description:
+      "Robe jaune dorée coiffée d'une épaisse mousse blanche onctueuse.",
+  },
+]
