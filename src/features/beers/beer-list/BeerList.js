8f966c86ae20d1f095af5376ca328b91ea2335b9
nicolas <n.gaget69@gmail.com>
2021-03-18 13:19:22 +0100

    Refactorisation de la méthode de sélection de film

--- src/features/beers/beer-list/BeerList.js
+++ src/features/beers/beer-list/BeerList.js
@@ -5,30 +5,12 @@ export default class BeerList extends Component {
   render() {
     return (
       <div className="w-75 d-flex flex-wrap justify-content-center">
-        <BeerElement
-          beer={this.props.beers[0]}
-          updateSelectedBeer={this.props.updateSelectedBeer}
-        />
-        <BeerElement
-          beer={this.props.beers[1]}
-          updateSelectedBeer={this.props.updateSelectedBeer}
-        />
-        <BeerElement
-          beer={this.props.beers[2]}
-          updateSelectedBeer={this.props.updateSelectedBeer}
-        />
-        <BeerElement
-          beer={this.props.beers[3]}
-          updateSelectedBeer={this.props.updateSelectedBeer}
-        />
-        <BeerElement
-          beer={this.props.beers[4]}
-          updateSelectedBeer={this.props.updateSelectedBeer}
-        />
-        <BeerElement
-          beer={this.props.beers[5]}
-          updateSelectedBeer={this.props.updateSelectedBeer}
-        />
+        {this.props.beers.map((m, index) => (
+      <BeerElement key={m.name + index}
+        beer={m}
+        updateSelectedBeer={() => { this.props.updateSelectedBeer(index) }} />
+    ))}
+        
       </div>
     );
   }
